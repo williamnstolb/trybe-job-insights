5be1e8c8f320de7958614ca26d88b1e68291bd1f
Williamns <williamnstolb@gmail.com>
2022-06-04 19:11:25 -0300

    Fix: valor da primeira posicao corrigido

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -6,4 +6,4 @@ def test_sort_by_criteria():
     path = 'src/jobs.csv'
     jobs = read(path)
     sort_by(jobs, 'min_salary')
-    assert jobs[1]['min_salary'] == '19875'
+    assert jobs[1]['min_salary'] == '19857'
