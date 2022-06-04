86c349d056d764f74126ede5d094f794da8314e8
Williamns <williamnstolb@gmail.com>
2022-06-04 18:57:39 -0300

    Fix: Corrigindo palavras em test brazilian jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,12 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    path = 'tests/mocks/brazilians_jobs.csv'
+    for job in read_brazilian_file(path):
+        assert 'title' in job
+        assert 'salary' in job
+        assert 'type' in job
+        assert 'titulo' not in job
+        assert 'salario' not in job
+        assert 'tipo' not in job
