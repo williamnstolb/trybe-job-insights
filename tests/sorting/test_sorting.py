4a944d8368f81f432cb2b1b6f8f5b0d071c6cbf5
Williamns <williamnstolb@gmail.com>
2022-06-04 19:16:17 -0300

    teste max_salary ok

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -5,6 +5,11 @@ from src.jobs import read
 def test_sort_by_criteria():
     path = 'src/jobs.csv'
     jobs = read(path)
+
     sort_by(jobs, 'min_salary')
     assert jobs[1]['min_salary'] == '19857'
     assert jobs[-2]['min_salary'] == ''
+
+    sort_by(jobs, 'max_salary')
+    assert jobs[1]['max_salary'] == '315439'
+    assert jobs[-2]['max_salary'] == ''
