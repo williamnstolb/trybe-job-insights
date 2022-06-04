88d046336a40a5bfe2d246ae7653b40a0a82fe8e
Williamns <williamnstolb@gmail.com>
2022-06-04 18:54:53 -0300

    Fix: valor para Java

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -4,4 +4,4 @@ from src.counter import count_ocurrences
 def test_counter():
     path = 'src/jobs.csv'
     assert count_ocurrences(path, 'Python') == 1639
-    assert count_ocurrences(path, 'Java') == 0
+    assert count_ocurrences(path, 'Java') == 676
