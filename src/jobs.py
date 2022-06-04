8659b35f4e600a41a9cf77624ff7d2d45d236a4d
Williamns <williamnstolb@gmail.com>
2022-06-04 17:36:28 -0300

    Implementando READ

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,17 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
+    list = []
+    with open(path, encoding="utf-8") as file:
+        readerFile = csv.reader(file, delimiter=",", quotechar='"')
+        header, *data = readerFile
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    for dataRow in data:
+        dataDict = {}
+        for index in range(len(header)):
+            dataDict[header[index]] = dataRow[index]
+        list.append(dataDict)
+    return list
