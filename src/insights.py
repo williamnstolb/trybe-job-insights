2455ed18b623e383965fbfa134cfebf5806b52d4
Williamns <williamnstolb@gmail.com>
2022-06-04 17:44:45 -0300

    Implementando get unique job types

--- src/insights.py
+++ src/insights.py
@@ -1,19 +1,13 @@
+from src.jobs import read
+
+
 def get_unique_job_types(path):
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    return []
+    jobList = read(path)
+    jobTypeList = list()
+    for job in jobList:
+        if job["job_type"] not in jobTypeList:
+            jobTypeList.append(job["job_type"])
+    return jobTypeList
 
 
 def filter_by_job_type(jobs, job_type):
