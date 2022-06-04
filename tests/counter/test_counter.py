273e81a11d9559e986957efd4be6d7d2e713b952
Williamns <williamnstolb@gmail.com>
2022-06-04 18:47:39 -0300

    Fix: Corrigindo teste

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -3,4 +3,5 @@ from src.counter import count_ocurrences
 
 def test_counter():
     path = 'src/jobs.csv'
-    assert count_ocurrences(path) == {'python': 1639, 'salary': 598, 'jobs': 306}
+    assert count_ocurrences(path, 'Python') == 1639
+    assert count_ocurrences(path, 'Java') == 0
