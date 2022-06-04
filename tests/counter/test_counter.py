a5d77b69ca4167cd8d8f54a7b1511df8f5381115
Williamns <williamnstolb@gmail.com>
2022-06-04 18:43:14 -0300

    Implementando test counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,6 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = 'src/jobs.csv'
+    assert count_ocurrences(path) == {'python': 1639, 'salary': 598, 'jobs': 306}
