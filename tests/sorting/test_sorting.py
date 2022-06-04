ed2889df83718d00dff6b662a5e4502e94b0bb75
Williamns <williamnstolb@gmail.com>
2022-06-04 19:09:29 -0300

    Implementando test sort by criteria

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,9 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+from src.jobs import read
 
 
 def test_sort_by_criteria():
-    pass
+    path = 'src/jobs.csv'
+    jobs = read(path)
+    sort_by(jobs, 'min_salary')
+    assert jobs[1]['min_salary'] == '19875'
